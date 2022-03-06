b17d3a28948083d7e22d7f4a18024ff6b5c66b94
N3k0Ch4n <95261992+N3k0Ch4n@users.noreply.github.com>
2022-03-06 16:36:16 +0800

    Delete tint2 directory

--- tint2/scripts/curinternet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env bash
-
-CUR=$(nmcli | grep "wlo1" | grep -v p2p | cut -d " " -f 4-)
-
-notify-send "Current Internet:" "$CUR"
-
--- tint2/scripts/curwindow.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-a=$(xprop -id $WINDOWID WM_CLASS | cut -d '"' -f 4)
-echo $a
