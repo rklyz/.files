a2d2a370a8b7966dae9fc898bfbcd4b04d55de81
N3k0Ch4n <95261992+N3k0Ch4n@users.noreply.github.com>
2022-03-17 23:51:07 +0800

    Delete scripts directory

--- scripts/mouseaccel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-ID=$(xinput list | grep [M]ouse | cut -d "=" -f 2 | cut -d "[" -f 1)
-
-echo $ID
-#xinput --set-prop $ID 'libinput Accel Profile Enabled' 0, 1 &
-
-xinput --set-prop $ID 'libinput Accel Speed' -1 &
-
