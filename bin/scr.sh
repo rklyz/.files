33ebcf887b201e26690423a8f2aeca321cc888c9
N3k0Ch4n <95261992+N3k0Ch4n@users.noreply.github.com>
2022-03-22 16:53:26 +0800

    Delete bin directory

--- bin/scr.sh
+++
@@ -1,5 +0,0 @@
-file=$(date +%C-%m-%d_%T).png
-loc=$HOME/Screenshot/
-notify-send "Screenshot in 3 seconds..." -t 2000
-maim -u -d 3 | tee $loc$file | xclip -selection clipboard -t image/png
-notify-send -i $loc$file "Screenshot Taken.."
