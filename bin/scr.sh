efcfb8a18524b54a64f9e525052c9a0678beaa52
N3k0Ch4n <95261992+N3k0Ch4n@users.noreply.github.com>
2022-03-23 10:58:24 +0800

    Update scr.sh

--- bin/scr.sh
+++ bin/scr.sh
@@ -1,5 +1,10 @@
 file=$(date +%C-%m-%d_%T).png
 loc=$HOME/Pictures/
-dunstify "Screenshot in 3 seconds..." -t 2000 -r "333"
-maim -u -d 3 | tee $loc$file | xclip -selection clipboard -t image/png
+dunstify "Screenshot in 3 seconds..." -t 1000 -r "333"
+sleep 1
+dunstify "Screenshot in 2 seconds..." -t 1000 -r "333"
+sleep 1
+dunstify "Screenshot in 1 seconds..." -t 1000 -r "333"
+sleep 1
+maim -u | tee $loc$file | xclip -selection clipboard -t image/png
 dunstify -i $loc$file "Screenshot Taken.." -t 5000 -r "333"
